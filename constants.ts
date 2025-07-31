69de893ff0f8651c057d035292369c628ce0cbf1
trongduc1805-ha <ducn9602@gmail.com>
2025-07-31 17:14:39 +0700

    constants.ts

--- constants.ts
+++ constants.ts
@@ -1,9 +1,8 @@
-
 import { Member } from './types';
 
 export const MEMBERS: Member[] = [
   "Ngoc Bao",
-  "Quang Chien",
+  "Quang Chien", 
   "Khac Dat",
   "Thien Duc",
   "Trong Duc",
@@ -18,15 +17,15 @@ export const MEMBERS: Member[] = [
 // Placeholder bank info for QR code generation.
 // Replace with actual bank IDs and account numbers.
 export const BANK_INFO: { [key: Member]: { bankId: string, accountNumber: string } } = {
-  "Ngọc Bảo": { bankId: "970407", accountNumber: "9999040906" },
-  "Quang Chiến": { bankId: "970436", accountNumber: "1028772903" },
-  "Khắc Đạt": { bankId: "970422", accountNumber: "896337379999" },
-  "Thiện Đức": { bankId: "970436", accountNumber: "104906767701" },
-  "Trọng Đức": { bankId: "970422", accountNumber: "0886861805" },
-  "Khánh Ngọc": { bankId: "970418", accountNumber: "5180989722" },
-  "Kim Khánh": { bankId: "970418", accountNumber: "5180989573" },
+  "Ngoc Bao": { bankId: "970407", accountNumber: "9999040906" },
+  "Quang Chien": { bankId: "970436", accountNumber: "1028772903" },
+  "Khac Dat": { bankId: "970422", accountNumber: "896337379999" },
+  "Thien Duc": { bankId: "970436", accountNumber: "104906767701" },
+  "Trong Duc": { bankId: "970422", accountNumber: "0886861805" },
+  "Khanh Ngoc": { bankId: "970418", accountNumber: "5180989722" },
+  "Kim Khanh": { bankId: "970418", accountNumber: "5180989573" },
   "Mai Trang": { bankId: "970405", accountNumber: "3609205495359" },
-  "Sử Uyên": { bankId: "970418", accountNumber: "8823712097" },
-  "Đức Thục": { bankId: "970422", accountNumber: "0705482788" },
-  "Ngọc Sơn": { bankId: "970418", accountNumber: "51810000989652" }
+  "Su Uyen": { bankId: "970418", accountNumber: "8823712097" },
+  "Duc Thuc": { bankId: "970422", accountNumber: "0705482788" },
+  "Ngoc Son": { bankId: "970418", accountNumber: "51810000989652" }
 };
